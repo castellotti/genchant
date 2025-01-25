aa3c44cacba5b63dee5626077c64eae1accde018
Steve Castellotti <sc@jigsaw.studio>
2025-01-24 22:28:00 -0800

    Integrate Godot XR Tools

--- Godot_Project/main.gd
+++ Godot_Project/main.gd
@@ -3,8 +3,6 @@ extends Node3D
 var xr_interface : XRInterface
 var passthrough_enabled : bool = false
 
-const TELEPORT_DEMO_SCENE_PATH = "res://scenes/teleport_demo/teleport_demo.tscn"
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	xr_interface = XRServer.find_interface("OpenXR")
@@ -19,9 +17,6 @@ func _ready():
 		passthrough_enabled = enable_passthrough()
 	else:
 		print("OpenXR not initialized, please check if your headset is connected")
-
-	#if not passthrough_enabled:
-		#load_teleport_demo()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
@@ -42,16 +37,3 @@ func enable_passthrough() -> bool:
 		get_viewport().transparent_bg = true
 
 	return true
-
-func load_teleport_demo():
-	# Load the teleport demo scene
-	var teleport_demo_scene = load(TELEPORT_DEMO_SCENE_PATH)
-
-	# Instance the teleport demo
-	var teleport_demo_instance = teleport_demo_scene.instantiate()
-
-	# Add the teleport demo instance as a child of the Main node
-	add_child(teleport_demo_instance)
-
-	# Optionally adjust the position or other properties if needed
-	#teleport_demo_instance.translation = Vector3(0, 0, 0)  # Adjust position if necessary
