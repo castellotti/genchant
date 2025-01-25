373ae50f71dce63f2b239dd3f00b2d7acdc084f8
Steve Castellotti <sc@jigsaw.studio>
2025-01-24 23:25:46 -0800

    Convert indentation from tabs to spaces

--- Godot_Project/assets/meshes/teleport/teleport.gd
+++ Godot_Project/assets/meshes/teleport/teleport.gd
@@ -5,17 +5,17 @@ extends Node3D
 
 ## Type of spawn-point data
 enum SpawnDataType {
-	## No data provided
-	NONE,
+    ## No data provided
+    NONE,
 
-	## Name of spawn-point node provided
-	NODE_NAME,
+    ## Name of spawn-point node provided
+    NODE_NAME,
 
-	## Vector3 of spawn-point provided
-	VECTOR3,
+    ## Vector3 of spawn-point provided
+    VECTOR3,
 
-	## Transform3D of spawn-point provided
-	TRANSFORM3D
+    ## Transform3D of spawn-point provided
+    TRANSFORM3D
 }
 
 
@@ -59,147 +59,147 @@ var _scene_base : XRToolsSceneBase
 
 
 func _ready():
-	_scene_base = XRTools.find_xr_ancestor(self, "*", "XRToolsSceneBase")
-	_update_title()
-	_update_teleport()
+    _scene_base = XRTools.find_xr_ancestor(self, "*", "XRToolsSceneBase")
+    _update_title()
+    _update_teleport()
 
 
 # Called when the player enters the teleport area
 func _on_TeleportArea_body_entered(body: Node3D):
-	# Skip if scene base is not known
-	if not _scene_base:
-		return
+    # Skip if scene base is not known
+    if not _scene_base:
+        return
 
-	# Skip if not the player body
-	if not body.is_in_group("player_body"):
-		return
+    # Skip if not the player body
+    if not body.is_in_group("player_body"):
+        return
 
-	# Skip if not active
-	if not active:
-		return
+    # Skip if not active
+    if not active:
+        return
 
-	# Teleport
-	if scene == "":
-		_scene_base.exit_to_main_menu()
-	elif spawn_data == SpawnDataType.NODE_NAME:
-		_scene_base.load_scene(scene, spawn_point_name)
-	elif spawn_data == SpawnDataType.VECTOR3:
-		_scene_base.load_scene(scene, spawn_point_position)
-	elif spawn_data == SpawnDataType.TRANSFORM3D:
-		_scene_base.load_scene(scene, spawn_point_transform)
-	else:
-		_scene_base.load_scene(scene)
+    # Teleport
+    if scene == "":
+        _scene_base.exit_to_main_menu()
+    elif spawn_data == SpawnDataType.NODE_NAME:
+        _scene_base.load_scene(scene, spawn_point_name)
+    elif spawn_data == SpawnDataType.VECTOR3:
+        _scene_base.load_scene(scene, spawn_point_position)
+    elif spawn_data == SpawnDataType.TRANSFORM3D:
+        _scene_base.load_scene(scene, spawn_point_transform)
+    else:
+        _scene_base.load_scene(scene)
 
 
 # Provide custom property information
 func _get_property_list() -> Array[Dictionary]:
-	# Return extra properties
-	return [
-		{
-			name = "Teleport",
-			type = TYPE_NIL,
-			usage = PROPERTY_USAGE_GROUP
-		},
-		{
-			name = "spawn_point_name",
-			type = TYPE_STRING,
-			usage = PROPERTY_USAGE_DEFAULT \
-					if spawn_data == SpawnDataType.NODE_NAME \
-					else PROPERTY_USAGE_NO_EDITOR
-		},
-		{
-			name = "spawn_point_position",
-			type = TYPE_VECTOR3,
-			usage = PROPERTY_USAGE_DEFAULT \
-					if spawn_data == SpawnDataType.VECTOR3 \
-					else PROPERTY_USAGE_NO_EDITOR
-		},
-		{
-			name = "spawn_point_transform",
-			type = TYPE_TRANSFORM3D,
-			usage = PROPERTY_USAGE_DEFAULT \
-					if spawn_data == SpawnDataType.TRANSFORM3D \
-					else PROPERTY_USAGE_NO_EDITOR
-		}
-	]
+    # Return extra properties
+    return [
+        {
+            name = "Teleport",
+            type = TYPE_NIL,
+            usage = PROPERTY_USAGE_GROUP
+        },
+        {
+            name = "spawn_point_name",
+            type = TYPE_STRING,
+            usage = PROPERTY_USAGE_DEFAULT \
+                    if spawn_data == SpawnDataType.NODE_NAME \
+                    else PROPERTY_USAGE_NO_EDITOR
+        },
+        {
+            name = "spawn_point_position",
+            type = TYPE_VECTOR3,
+            usage = PROPERTY_USAGE_DEFAULT \
+                    if spawn_data == SpawnDataType.VECTOR3 \
+                    else PROPERTY_USAGE_NO_EDITOR
+        },
+        {
+            name = "spawn_point_transform",
+            type = TYPE_TRANSFORM3D,
+            usage = PROPERTY_USAGE_DEFAULT \
+                    if spawn_data == SpawnDataType.TRANSFORM3D \
+                    else PROPERTY_USAGE_NO_EDITOR
+        }
+    ]
 
 
 # Allow revert of custom properties
 func _property_can_revert(property : StringName) -> bool:
-	match property:
-		"spawn_point_name":
-			return true
-		"spawn_point_position":
-			return true
-		"spawn_point_transform":
-			return true
-		_:
-			return false
+    match property:
+        "spawn_point_name":
+            return true
+        "spawn_point_position":
+            return true
+        "spawn_point_transform":
+            return true
+        _:
+            return false
 
 
 # Provide revert values for custom properties
 func _property_get_revert(property : StringName): # Variant
-	match property:
-		"spawn_point_name":
-			return ""
-		"spawn_point_position":
-			return Vector3.ZERO
-		"spawn_point_transform":
-			return Transform3D.IDENTITY
+    match property:
+        "spawn_point_name":
+            return ""
+        "spawn_point_position":
+            return Vector3.ZERO
+        "spawn_point_transform":
+            return Transform3D.IDENTITY
 
 
 func set_collision_disabled(value):
-	if !Engine.is_editor_hint():
-		for child in get_node("TeleportBody").get_children():
-			if child is CollisionShape3D:
-				child.disabled = value
+    if !Engine.is_editor_hint():
+        for child in get_node("TeleportBody").get_children():
+            if child is CollisionShape3D:
+                child.disabled = value
 
 
 func _set_spawn_data(p_spawn_data : SpawnDataType) -> void:
-	spawn_data = p_spawn_data
-	notify_property_list_changed()
+    spawn_data = p_spawn_data
+    notify_property_list_changed()
 
 
 func _set_title(value):
-	title = value
-	if is_inside_tree():
-		_update_title()
+    title = value
+    if is_inside_tree():
+        _update_title()
 
 
 func _update_title():
-	if title:
-		var material: ShaderMaterial = $TeleportBody/Top.get_active_material(1)
-		material.set_shader_parameter("Title", title)
+    if title:
+        var material: ShaderMaterial = $TeleportBody/Top.get_active_material(1)
+        material.set_shader_parameter("Title", title)
 
 
 func _set_active(value):
-	active = value
-	if is_inside_tree():
-		_update_teleport()
+    active = value
+    if is_inside_tree():
+        _update_teleport()
 
 
 func _set_active_beam_color(value):
-	active_beam_color = value
-	if is_inside_tree():
-		_update_teleport()
+    active_beam_color = value
+    if is_inside_tree():
+        _update_teleport()
 
 
 func _set_inactive_beam_color(value):
-	inactive_beam_color = value
-	if is_inside_tree():
-		_update_teleport()
+    inactive_beam_color = value
+    if is_inside_tree():
+        _update_teleport()
 
 
 func _set_inactive_beam_visible(value):
-	inactive_beam_visible = value
-	if is_inside_tree():
-		_update_teleport()
+    inactive_beam_visible = value
+    if is_inside_tree():
+        _update_teleport()
 
 
 func _update_teleport():
-	if active:
-		$TeleportArea/Cylinder.get_surface_override_material(0).set_shader_parameter("beam_color", active_beam_color)
-		$TeleportArea/Cylinder.visible = true
-	else:
-		$TeleportArea/Cylinder.get_surface_override_material(0).set_shader_parameter("beam_color", inactive_beam_color)
-		$TeleportArea/Cylinder.visible = inactive_beam_visible
+    if active:
+        $TeleportArea/Cylinder.get_surface_override_material(0).set_shader_parameter("beam_color", active_beam_color)
+        $TeleportArea/Cylinder.visible = true
+    else:
+        $TeleportArea/Cylinder.get_surface_override_material(0).set_shader_parameter("beam_color", inactive_beam_color)
+        $TeleportArea/Cylinder.visible = inactive_beam_visible
--- Godot_Project/openxr_staging.gd
+++ Godot_Project/openxr_staging.gd
@@ -30,44 +30,44 @@ var control_pad_hand : String = "LEFT"
 
 
 func _ready() -> void:
-	# In Godot 4 we must now manually call our super class ready function
-	super()
+    # In Godot 4 we must now manually call our super class ready function
+    super()
 
 
 func _on_Staging_scene_loaded(_scene, _user_data):
-	# We only show the press to continue the first time we load a scene
-	# to give the player time to put their headset on.
-	prompt_for_continue = false
-	scene_is_loaded = true
+    # We only show the press to continue the first time we load a scene
+    # to give the player time to put their headset on.
+    prompt_for_continue = false
+    scene_is_loaded = true
 
 
 func _on_Staging_scene_exiting(_scene, _user_data):
-	# We no longer have an active scene
-	scene_is_loaded = false
+    # We no longer have an active scene
+    scene_is_loaded = false
 
 
 func _on_Staging_xr_started():
-	# We get the 'xr_started' signal when the user puts on their headset,
-	# or returns from the system menus.
-	# If the user did so while we were already scene switching
-	# we leave our prompt for continue on,
-	# else we turn our prompt for continue off.
-	if scene_is_loaded:
-		# No longer need our prompt
-		prompt_for_continue = false
+    # We get the 'xr_started' signal when the user puts on their headset,
+    # or returns from the system menus.
+    # If the user did so while we were already scene switching
+    # we leave our prompt for continue on,
+    # else we turn our prompt for continue off.
+    if scene_is_loaded:
+        # No longer need our prompt
+        prompt_for_continue = false
 
-		# This would be a good moment to unpause your game
+        # This would be a good moment to unpause your game
 
 
 func _on_Staging_xr_ended():
-	# We get the 'xr_ended' whenever the player removes their headset (or goes
-	# into the menu system).
-	#
-	# If the user doesn't put their headset on again before we load a
-	# new scene, we'll want to show the prompt so we don't load the
-	# next scene in while the player is still adjusting their position
-	prompt_for_continue = true
+    # We get the 'xr_ended' whenever the player removes their headset (or goes
+    # into the menu system).
+    #
+    # If the user doesn't put their headset on again before we load a
+    # new scene, we'll want to show the prompt so we don't load the
+    # next scene in while the player is still adjusting their position
+    prompt_for_continue = true
 
-	if scene_is_loaded:
-		# This would be a good moment to pause your game
-		pass
+    if scene_is_loaded:
+        # This would be a good moment to pause your game
+        pass
